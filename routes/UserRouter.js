3b5400440168dcbaeb09c7b7354366dc76026bd2
azputra <ahmadzp102@gmail.com>
2019-12-18 20:03:55 +0700

    update views admin

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -8,7 +8,7 @@ router.get('/admin', UserController.getUsersAll);
 router.get('/add', UserController.getUsersAdd);
 router.post('/add', UserController.postUsersAdd);
 router.get('/edit/:id', UserController.getUsersEdit);
-router.post('/edit/:id', UserController.postUsersAdd);
+router.post('/edit/:id', UserController.postUsersEdit);
 router.get('/delete/:id', UserController.getUsersDelete);
 
 module.exports = router;
